75daaa9dcd669bef289104b47960a1adc3a4945d
Paweł Chudzik <pawel.chudzik@gmail.com>
2016-09-22 22:26:01 +0200

    ugly fixes to make frontend usable

--- public/script.js
+++ public/script.js
@@ -3,18 +3,13 @@
 initialize();
 
 var host = window.document.location.host.replace(/:.*/, '');
-var showDates = false;
-var searchRegexp = new RegExp('.*');
 var webSocket;
+var follow = true;
 
 function initialize() {
 	$(document).ready(function () {
 		loadInputs();
 	});
-
-	$('#search').keyup(_.debounce(function () {
-		filter($('#search').val());
-	}, 300));
 }
 
 function loadInputs() {
@@ -37,7 +32,8 @@ function loadInputs() {
 
 function startWatching(eventSource) {
 	stopWatching();
-	logContainer().innerHTML = '';
+	logContainer().show();
+	logContainer().html('');
 	webSocket = new WebSocket('ws://' + host + ':' + (window.location.port || 80) + '/' + eventSource);
 	webSocket.onmessage = function (event) {
 		appendToLog(event.data);
@@ -59,8 +55,10 @@ function isWatching() {
 function appendToLog(eventsJSON) {
 	var events = JSON.parse(eventsJSON);
 	logContainer().append(printEvent(events));
-	scrollToBottom(200 * events.length);
-};
+	if(canFollow()) {
+		scrollToBottom(200 * events.length);
+	}
+}
 
 function scrollToBottom(scrollTime) {
 	if (scrollTime > 1000) {
@@ -71,51 +69,31 @@ function scrollToBottom(scrollTime) {
 }
 
 function logContainer() {
-	return $('#log');
+	return $('#log').find('pre');
 }
 
 function printEvent(event) {
-	var $item = $(document.createElement('p')).attr('class', 'log-item');
-
-	if (event.sourceName) {
-		$item.append('<span class="label label-default source-name">' + event.sourceName + '</span>');
-	}
-
-	var displayProperty = showDates ? 'inline' : 'none';
-	$item.append('<span class="badge event-date" style="display:' + displayProperty + ';">' + new Date(event.timestamp) + '</span>');
-	$item.append('<span class="log">' + event.data + '</span>');
-	isVisible($item, searchRegexp);
-	return $item;
+	return safeTagsReplace(event.data);
 }
 
-function toggleDates() {
-	showDates = !showDates;
-	$('.event-date').toggle();
+var tagsToReplace = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;'
+};
+
+function replaceTag(tag) {
+	return tagsToReplace[tag] || tag;
 }
 
-function filter(searchText) {
-	if (_.trim(searchText) === '') {
-		searchRegexp = new RegExp('.*');
-		$('.log-item').each(function (index, element) {
-			isVisible($(element), searchRegexp);
-		});
-		return;
-	}
-
-	searchRegexp = new RegExp(searchText);
-	$('.log-item').each(function (index, element) {
-		isVisible($(element), searchRegexp);
-	});
+function safeTagsReplace(str) {
+	return str.replace(/[&<>]/g, replaceTag);
 }
 
-function isVisible($element, search) {
-	var sourceName = _.lowerCase($element.find('.source-name').text());
-	var dataText = _.lowerCase($element.find('.log').text());
+function canFollow() {
+	return follow;
+}
 
-
-	if (!(search.test(sourceName) || search.test(dataText))) {
-		$element.hide();
-	} else {
-		$element.show();
-	}
+function toggleFollowing() {
+	follow = !follow;
 }
