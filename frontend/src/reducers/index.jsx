07d08f49528dfe0fc46f478688d234a52d489021
Paweł Chudzik <pawel.chudzik@gmail.com>
2016-10-04 21:44:10 +0200

    fixup build

--- frontend/src/reducers/index.jsx
+++ frontend/src/reducers/index.jsx
@@ -1,8 +1,6 @@
 import {combineReducers} from "redux";
 import messages from "../message/messageReducer";
-import providers from "../home/providers/providersReducer";
 
 export default combineReducers({
-	messages,
-	providers
+	messages
 });
