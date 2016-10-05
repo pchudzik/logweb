9b3ae672768374bbbbd29735d004e72f72d70682
Paweł Chudzik <pawel.chudzik@gmail.com>
2016-10-05 21:33:27 +0200

    providers reducer

---
+++ frontend/src/home/providers/providersReducer.jsx
@@ -0,0 +1,49 @@
+import {
+	FETCH_PROVIDERS_PENDING,
+	FETCH_PROVIDERS_FULFILLED,
+	FETCH_PROVIDERS_REJECTED
+} from "./providersActions";
+
+const defaultProvidersState = {
+	providers: [],
+	request: {
+		fetching: false,
+		fetched: false,
+		error: null
+	}
+};
+
+export default function providersFetchReducer(state = defaultProvidersState, action) {
+	switch (action.type) {
+		case FETCH_PROVIDERS_PENDING:
+			return {
+				...state,
+				request: {
+					fetching: true,
+					fetched: false,
+					error: null
+				}
+			};
+		case FETCH_PROVIDERS_FULFILLED:
+			return {
+				...state,
+				providers: action.payload,
+				request: {
+					fetching: false,
+					fetched: true,
+					error: null
+				}
+			};
+		case FETCH_PROVIDERS_REJECTED:
+			return {
+				...state,
+				request: {
+					fetching: false,
+					fetched: false,
+					error: action.payload
+				}
+			};
+		default:
+			return state;
+	}
+}
---
+++ frontend/src/home/providers/providersReducer.spec.jsx
@@ -0,0 +1,96 @@
+import {expect} from "chai";
+import providersReducer from "./providersReducer";
+import {
+	FETCH_PROVIDERS_PENDING,
+	FETCH_PROVIDERS_FULFILLED,
+	FETCH_PROVIDERS_REJECTED
+} from "./providersActions";
+
+describe("providersReducer.spec.js", () => {
+	const oldProviders = ["old", "providers", "collection"];
+
+	it("should return default state", () => {
+		expect(providersReducer(undefined, "unknown action")).to.eql({
+			providers: [],
+			request: {
+				fetching: false,
+				fetched: false,
+				error: null
+			}
+		});
+	});
+
+	it(`should handle ${FETCH_PROVIDERS_PENDING} action`, () => {
+		expect(
+			providersReducer({
+				providers: oldProviders,
+				request: {
+					fetching: false,
+					fetched: true,
+					error: "any error"
+				}
+			}, {type: FETCH_PROVIDERS_PENDING})
+		).to.eql(
+			{
+				providers: oldProviders,
+				request: {
+					fetching: true,
+					fetched: false,
+					error: null
+				}
+			}
+		);
+	});
+
+	it(`should handle ${FETCH_PROVIDERS_FULFILLED} action`, () => {
+		// given
+		const newProviders = ["new", "providers", "collection"];
+
+		// expect
+		expect(
+			providersReducer({
+				providers: oldProviders,
+				request: {
+					fetching: true,
+					fetched: false,
+					error: "any error"
+				}
+			}, {type: FETCH_PROVIDERS_FULFILLED, payload: newProviders})
+		).to.eql(
+			{
+				providers: newProviders,
+				request: {
+					fetching: false,
+					fetched: true,
+					error: null
+				}
+			}
+		);
+	});
+
+	it(`should handle ${FETCH_PROVIDERS_REJECTED} action`, () => {
+		// given
+		const error = "fetch providers failure!";
+
+		// expect
+		expect(
+			providersReducer({
+				providers: oldProviders,
+				request: {
+					fetching: false,
+					fetched: true,
+					error: null
+				}
+			}, {type: FETCH_PROVIDERS_REJECTED, payload: error})
+		).to.eql(
+			{
+				providers: oldProviders,
+				request: {
+					error,
+					fetching: false,
+					fetched: false
+				}
+			}
+		);
+	});
+});
