792530ebf07ebe14f15727d1290ceaa06abded06
Paweł Chudzik <pawel.chudzik@gmail.com>
2016-10-09 22:00:15 +0200

    complete frontend build

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,47 @@
-const debug = process.env.NODE_ENV !== "production";
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fs = require("fs");
 const path = require("path");
+const webpack = require("webpack");
+const htmlTemplate = require("html-webpack-template");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");
+
+const debug = process.env.NODE_ENV !== "production";
+const commonPlugins = [
+	new ProgressBarPlugin({
+		format: "  build [:bar] :percent - :msg (:elapsed seconds)"
+	}),
+	new webpack.optimize.OccurenceOrderPlugin(),
+	new HtmlWebpackPlugin({
+		inject: false,
+		template: htmlTemplate,
+		appMountId: "app",
+		title: "Logweb"
+	})
+];
+
+const vendorsToSkip = ["font-awesome"];
+const vendorModules = Object.keys(JSON.parse(fs.readFileSync("package.json")).dependencies)
+	.filter(module => !vendorsToSkip.includes(module));
 
 module.exports = {
 	context: path.join(__dirname, "src"),
 	devtool: debug ? "inline-sourcemap" : null,
-	entry: "./app.jsx",
+	entry: {
+		vendor: vendorModules,
+		vendorCss: ["./vendor.scss"],
+		app: "./app.jsx"
+	},
+	output: {
+		path: path.join(__dirname, "../backend/public"),
+		filename: "[name].js"
+	},
+	devServer: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:8008"
+			}
+		}
+	},
 	resolve: {
 		extensions: ["", ".js", ".jsx"]
 	},
@@ -20,6 +55,14 @@ module.exports = {
 			{
 				test: /\.scss$/,
 				loaders: ["style", "css?sourceMap", "sass?sourceMap"]
+			},
+			{
+				test: require.resolve("jquery"),
+				loader: "expose?jQuery!expose?$"
+			},
+			{
+				test: /bootstrap-sass\/assets\/javascripts\//,
+				loader: "imports?jQuery=jquery"
 			},
 			{
 				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
@@ -43,24 +86,19 @@ module.exports = {
 			}
 		]
 	},
-	output: {
-		path: path.join(__dirname, "/build"),
-		filename: "app.min.js"
-	},
-	plugins: debug ? [
-		new HtmlWebpackPlugin({template: "index.html.template"}),
+	plugins: debug ? commonPlugins.concat([
 		new webpack.DefinePlugin({
 			"process.env.BACKEND_PORT": JSON.stringify(process.env.BACKEND_PORT)
-		}),
+		})
+	]) : commonPlugins.concat([
 		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.OccurenceOrderPlugin()
-	] : [],
-	devServer: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:8008"
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
 			}
-		}
-	}
-}
-;
+		}),
+		new webpack.DefinePlugin({
+			"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+		})
+	])
+};
